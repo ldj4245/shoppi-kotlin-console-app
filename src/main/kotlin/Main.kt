1f909d4bb073b0d27c6d2e19976c19431336a8cb
idaejin <ldj4241@naver.com>
2022-09-11 16:49:11 +0900

    실습 1. 인사말과 사용자에게 입력 받은 정보 출력

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+
+    println("안녕하세요, 환영해요")
+    println("이름을 입력해주세요")
+
+    val name = readLine()
+
+    println("""
+        감사합니다. 반갑습니다 $name 님
+        원하시는 카테고리를 입력해주세요.
+    """.trimIndent())
+
+
+
 }
